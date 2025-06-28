b74e69926061a52e9f28dc93427378fc3f48a1aa
Maxim Betin <betinmaxim@gmail.com>
2025-06-28 11:19:59 +0200

    fix: increase BROWSER_LOAD_DELAY and INSTAGRAM_ACCOUNT_LOAD_DELAY to improve loading times

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ TIMEZONE = timezone(timedelta(hours=int(os.getenv('TIMEZONE_OFFSET', '2'))))
 
 # Browser settings
 BROWSER_DEBUG_PORT = int(os.getenv('BROWSER_DEBUG_PORT', '9222'))
-BROWSER_LOAD_DELAY = int(os.getenv('BROWSER_LOAD_DELAY', '3'))  # seconds
+BROWSER_LOAD_DELAY = int(os.getenv('BROWSER_LOAD_DELAY', '5'))  # seconds
 BROWSER_LOAD_TIMEOUT = int(os.getenv('BROWSER_LOAD_TIMEOUT', '10000'))  # milliseconds
 BROWSER_PATH = os.getenv('BROWSER_PATH', os.path.expandvars(r"%PROGRAMFILES%\BraveSoftware\Brave-Browser\Application\brave.exe"))
 
@@ -16,7 +16,7 @@ BROWSER_PATH = os.getenv('BROWSER_PATH', os.path.expandvars(r"%PROGRAMFILES%\Bra
 INSTAGRAM_URL = os.getenv('INSTAGRAM_URL', "https://www.instagram.com/")
 INSTAGRAM_MAX_POST_AGE = int(os.getenv('INSTAGRAM_MAX_POST_AGE', '1'))  # days
 INSTAGRAM_POST_LOAD_DELAY = int(os.getenv('INSTAGRAM_POST_LOAD_DELAY', '2'))  # seconds
-INSTAGRAM_ACCOUNT_LOAD_DELAY = int(os.getenv('INSTAGRAM_ACCOUNT_LOAD_DELAY', '1'))  # seconds
+INSTAGRAM_ACCOUNT_LOAD_DELAY = int(os.getenv('INSTAGRAM_ACCOUNT_LOAD_DELAY', '2'))  # seconds
 INSTAGRAM_MAX_POSTS_PER_ACCOUNT = int(os.getenv('INSTAGRAM_MAX_POSTS_PER_ACCOUNT', '2'))  # posts
 
 # Instagram accounts to track
