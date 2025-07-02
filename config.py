896043e674326bae5b4d633d3ab064b277165594
Maxim Betin <betinmaxim@gmail.com>
2025-07-02 12:32:28 +0200

    fix: update INSTAGRAM_MAX_POST_AGE to 3 days and increase INSTAGRAM_MAX_POSTS_PER_ACCOUNT to 3

--- config.py
+++ config.py
@@ -14,10 +14,10 @@ BROWSER_PATH = os.getenv('BROWSER_PATH', os.path.expandvars(r"%PROGRAMFILES%\Bra
 
 # Instagram settings
 INSTAGRAM_URL = os.getenv('INSTAGRAM_URL', "https://www.instagram.com/")
-INSTAGRAM_MAX_POST_AGE = int(os.getenv('INSTAGRAM_MAX_POST_AGE', '2'))  # days
+INSTAGRAM_MAX_POST_AGE = int(os.getenv('INSTAGRAM_MAX_POST_AGE', '3'))  # days
 INSTAGRAM_POST_LOAD_DELAY = int(os.getenv('INSTAGRAM_POST_LOAD_DELAY', '3'))  # seconds
 INSTAGRAM_ACCOUNT_LOAD_DELAY = int(os.getenv('INSTAGRAM_ACCOUNT_LOAD_DELAY', '3'))  # seconds
-INSTAGRAM_MAX_POSTS_PER_ACCOUNT = int(os.getenv('INSTAGRAM_MAX_POSTS_PER_ACCOUNT', '2'))  # posts
+INSTAGRAM_MAX_POSTS_PER_ACCOUNT = int(os.getenv('INSTAGRAM_MAX_POSTS_PER_ACCOUNT', '3'))  # posts
 
 # Instagram accounts to track
 INSTAGRAM_ACCOUNTS = [
